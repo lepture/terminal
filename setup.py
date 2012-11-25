9cb4675a484b5c91beaba34bc7fecb8ce1aa0998
Hsiaoming Yang <lepture@me.com>
2012-11-25 21:28:56 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+kwargs = {}
+major, minor = sys.version_info[:2]
+if major >= 3:
+    kwargs['use_2to3'] = True
+
+from setuptools import setup
+
+
+import terminal
+from email.utils import parseaddr
+author, author_email = parseaddr(terminal.__author__)
+
+setup(
+    name='terminal',
+    version=terminal.__version__,
+    author=author,
+    author_email=author_email,
+    url=terminal.__homepage__,
+    packages=['terminal'],
+    description=terminal.__doc__,
+    long_description=open('README.rst').read(),
+    license='BSD License',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    **kwargs
+)
--- terminal/__init__.py
+++ terminal/__init__.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 
+"""
+A terminal environment tools.
+"""
+
+
 from .color import *
 from .logger import Logger
 from .progress import Progress
+
+
+__author__ = 'Hsiaoming Yang <lepture@me.com>'
+__version__ = '0.0.1'
+__homepage__ = 'http://lab.lepture.com/terminal/'
 
 
 logging = Logger(
