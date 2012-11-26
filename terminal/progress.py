c4eaff412eb7d585b33a668c81c3c1e1d960a4ed
Hsiaoming Yang <lepture@me.com>
2012-11-26 11:21:07 +0800

    make progress works well

--- terminal/progress.py
+++ terminal/progress.py
@@ -3,20 +3,21 @@ import os
 
 
 def get_terminal_width():
-    "Borrowed from the py lib."
+    """Borrowed from the py lib."""
     try:
         import termios
         import fcntl
         import struct
         call = fcntl.ioctl(0, termios.TIOCGWINSZ,
                            struct.pack('hhhh', 0, 0, 0, 0))
-        _, width = struct.upack('hhhh', call)[:2]
-        return width
+        height, width = struct.unpack('hhhh', call)[:2]
+        terminal_width = width
     except (SystemExit, KeyboardInterrupt):
         raise
     except:
-        # Fallback
-        return int(os.environ.get('COLUMNS', 80))
+        # FALLBACK
+        terminal_width = int(os.environ.get('COLUMNS', 80)) - 1
+    return terminal_width
 
 
 class Progress(object):
@@ -24,21 +25,22 @@ class Progress(object):
         self.marker = marker
         self.left = left
         self.right = right
-        if len(fill) == 0:
-            raise ValueError('You must fill something.')
+        if len(fill) != 1:
+            raise ValueError('You must a char.')
         self.fill = fill
         self.width = get_terminal_width()
 
     def __call__(self, current, total, blank='', status=''):
         if current > total:
             raise ValueError("current can't be larger than total.")
-        marker = int(self.width / float(total)) * current * self.marker
-        marker = self.left + marker + self.right
-        count = len(blank) + len(status)
+        count = len(blank) + len(status) + len(self.left) + len(self.right)
+
+        marker = int((self.width - count) / float(total) * current) * \
+                self.marker
         if len(marker) >= self.width - count:
-            split = self.width - count - len(self.right)
-            marker = marker[:split] + self.right
-        to_fill = self.width - len(marker) - count
-        fill = ((to_fill - 1) / len(self.fill) + 1) * self.fill
-        fill = fill[:to_fill]
-        return blank + marker + fill + status
+            marker = marker[:self.width - count]
+
+        bar = blank + self.left + marker + self.right
+        text = status.rjust(self.width, self.fill)
+        text = text.replace(self.fill * len(bar), bar, 1)
+        return text
