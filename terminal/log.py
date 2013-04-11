704ae6ee715cee37e99d808c629c8f0459790c0d
Hsiaoming Yang <me@lepture.com>
2013-04-12 00:25:57 +0800

    use log

---
+++ terminal/log.py
@@ -0,0 +1,73 @@
+import sys
+
+
+class Logger(object):
+    def __init__(self, enable_verbose=False):
+        self._enable_verbose = enable_verbose
+        self._indent = 0
+
+    def config(self, **kwargs):
+        self._indent = kwargs.get('indent', 0)
+        self._enable_verbose = kwargs.get('verbose', False)
+        self._enable_quiet = kwargs.get('quiet', False)
+
+    def message(self, level, *args):
+        # rewrite this method to format your own message
+        from . import color
+        msg = ' '.join(args)
+        if level == 'start':
+            return color.magenta('=> ') + msg
+        if level == 'end':
+            return color.magenta('* ') + msg
+        m = {
+            'debug': 'gray',
+            'info': 'green',
+            'warn': 'yellow',
+            'error': 'red'
+        }
+        if level in m:
+            fn = getattr(color, m[level])
+            return '%s: %s' % (fn(level), msg)
+        return msg
+
+    def writeln(self, level='info', *args):
+        if not self._enable_verbose and self._is_verbose:
+            return self
+        msg = self.message(level, *args)
+        if self._indent:
+            msg = '  ' * self._indent + msg
+        if level == 'error':
+            sys.stderr.write(msg + '\n')
+        else:
+            sys.stdout.write(msg + '\n')
+        return self
+
+    @property
+    def verbose(self):
+        self._is_verbose = True
+        return self
+
+    def start(self, *args):
+        self.writeln('start', *args)
+        self._indent += 1
+        return self
+
+    def end(self, *args):
+        self._indent -= 1
+        return self.writeln('end', *args)
+
+    def debug(self, *args):
+        if self._enable_quiet:
+            return self
+        return self.writeln('debug', *args)
+
+    def info(self, *args):
+        if self._enable_quiet:
+            return self
+        return self.writeln('info', *args)
+
+    def warn(self, *args):
+        return self.writeln('warn', *args)
+
+    def error(self, *args):
+        return self.writeln('error', *args)
